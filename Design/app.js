b16ec80aa095c76261af7281deba8ac4b97862b7
SandyCode123 <sandip.instahelpers@gmail.com>
2016-12-01 09:50:18 -0800

    Add files via upload

---
+++ Design/app.js
@@ -0,0 +1,27 @@
+angular.module('app', ['ui.router'])
+  .config([
+    '$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+    .state('home', {
+      url: '/',
+      views: {
+        '': {
+          templateUrl: './templates/main.html'
+        },
+        'nav@home': {
+          templateUrl: './templates/assets/nav.html'
+        },
+        'body@home': {
+          templateUrl: './templates/body.html'
+        },
+        'footer@home': {
+          templateUrl: './templates/assets/footer.html'
+        }
+      }
+    }).state('home.postaquestion', {
+           url: 'postaquestion',
+		   templateUrl: './templates/pages/postquestion.html'	
+		    }
+        );
+  }]);
