45e233776b8206eae6b6d17bae4c1ee0340522bb
SandyCode123 <sandip.instahelpers@gmail.com>
2016-12-12 00:03:33 +0530

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,14 @@
 var instaModule=angular.module('instahelpers', ['ngMaterial','ngAria','ngAnimate', 'ngMessages', 'material.svgAssetsCache']);
-instaModule.controller('mainController',['$scope','$http','$timeout', '$q',function($scope,$http,$timeout,$q){
+instaModule.controller('mainController',['$scope','$http','$timeout', '$q','$mdDialog',function($scope,$http,$timeout,$q,$mdDialog){
 	
 	$scope.clientPartSelected=false;
 	$scope.developerPartSelected=false;
 	$scope.clientConnectionPart=false;
 	$scope.developerInstaFilterPart=false;
-	// Skill selection chip code
+	$scope.InstaListShow=false;
 	
+	// Angular Material Chips Section
     var self = this;
-
     this.readonly = false;
     this.selectedItem = null;
     this.searchText = null;
@@ -87,7 +87,7 @@ instaModule.controller('mainController',['$scope','$http','$timeout', '$q',funct
     }
   
   
-    // on click of client selection button
+    // Button Event handlers
 	$scope.clientButtonClick=function(){
 		$scope.clientPartSelected=true;
 		$scope.developerPartSelected=false;
@@ -95,19 +95,179 @@ instaModule.controller('mainController',['$scope','$http','$timeout', '$q',funct
 	
 	$scope.clientNext=function(){
 		$scope.clientConnectionPart=true;
+		setTimeout(function(){
+			$('.cssload-container').fadeIn(500);
+		}, 2000);
 	}
 	
 	$scope.developerButtonClick=function(){
 		$scope.developerPartSelected=true;
 		$scope.clientPartSelected=false;
+		setTimeout(function(){
+			$('.cssload-container').fadeIn(500);
+		}, 2000);
 	}
   
 	$scope.developerNext=function(option){
 		if(option==1){
 			$scope.developerInstaFilterPart=true;
 		}else if(option==2){
-			//$scope.developerInstaFilterPart=true;
+			$scope.InstaListShow=true;
 		}
 	}
+	
+	// Signup Tab
+	$scope.showAdvanced = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'dialog1.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+  
+  // Controller for Signup Page.
+  function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }
+  
+  //  for Insta Search Page Radio Button
+  
+	    $scope.data = {
+		  group1 : 'All'
+		};
+
+		$scope.avatarData = [{
+			id: "avatars:svg-1",
+			title: 'avatar 1',
+			value: 'avatar-1'
+		  },{
+			id: "avatars:svg-2",
+			title: 'avatar 2',
+			value: 'avatar-2'
+		  },{
+			id: "avatars:svg-3",
+			title: 'avatar 3',
+			value: 'avatar-3'
+		}];
+
+		$scope.radioData = [
+		  { label: '1', value: 1 },
+		  { label: '2', value: 2 },
+		  { label: '3', value: '3', isDisabled: true },
+		  { label: '4', value: '4' }
+		];
+
+
+		$scope.submit = function() {
+		  alert('submit');
+		};
+
+		$scope.addItem = function() {
+		  var r = Math.ceil(Math.random() * 1000);
+		  $scope.radioData.push({ label: r, value: r });
+		};
+
+		$scope.removeItem = function() {
+		  $scope.radioData.pop();
+		};
+
+	
+  
+  
+  // Angular List for Insta Search By Developer.
+   var imagePath = 'img/list/60.jpeg';
+
+    $scope.phones = [
+      {
+        type: 'Home',
+        number: '(555) 251-1234',
+        options: {
+          icon: 'communication:phone'
+        }
+      },
+      {
+        type: 'Cell',
+        number: '(555) 786-9841',
+        options: {
+          icon: 'communication:phone',
+          avatarIcon: true
+        }
+      },
+      {
+        type: 'Office',
+        number: '(555) 314-1592',
+        options: {
+          face : imagePath
+        }
+      },
+      {
+        type: 'Offset',
+        number: '(555) 192-2010',
+        options: {
+          offset: true,
+          actionIcon: 'communication:phone'
+        }
+      }
+    ];
+    $scope.todos = [
+      {
+        face : imagePath,
+        skills: 'Java, Wordpress',
+		InstaLanguage: 'Hindi',
+        topic: 'I want to create plugin',
+        InstaDuration: '3.08 Hrs',
+		activeParticipants: 2
+      },
+      {
+        face : imagePath,
+        skills: 'Java, Wordpress',
+		InstaLanguage: 'Hindi',
+        topic: 'I want to create plugin',
+        InstaDuration: '3.08 Hrs',
+		activeParticipants: 2
+      },
+      {
+       face : imagePath,
+        skills: 'Java, Wordpress',
+		InstaLanguage: 'Hindi',
+        topic: 'I want to create plugin',
+        InstaDuration: '3.08 Hrs',
+		activeParticipants: 2
+      },
+      {
+        face : imagePath,
+        skills: 'Java, Wordpress',
+		InstaLanguage: 'Hindi',
+        topic: 'I want to create plugin',
+        InstaDuration: '3.08 Hrs',
+		activeParticipants: 2
+      },
+      {
+        face : imagePath,
+        skills: 'Java, Wordpress',
+		InstaLanguage: 'Hindi',
+        topic: 'I want to create plugin',
+        InstaDuration: '3.08 Hrs',
+		activeParticipants: 2
+      },
+    ];
   
 }])
